d195d81027d78de86664a9b50a1c69ce2fdcfaf5
Pavel Ivchenko <pavel.ivchenko.js@mail.ru>
2020-10-10 00:49:03 +0300

    feat: done calculateDepth

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,8 +1,16 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  calculateDepth(arr, depth = 1) {
+
+    return arr.reduce((accum, current) => {
+      return Array.isArray(current) ? Math.max(this.calculateDepth(current, depth + 1), accum) : accum;
+    }, depth);
+
   }
+
 };
+
+
+
+
--- src/what-season.js
+++ src/what-season.js
@@ -4,9 +4,6 @@ module.exports = function getSeason(date) {
   if (!date) return 'Unable to determine the time of year!';
   if ( !(date instanceof Date) || date.toString !== new Date(1990).toString)  throw new Error('THROWN');
 
-
-
-
   const month = new Date(date).getMonth();
   if (month >= 0 && month <= 1 || month === 11) return "winter";
   if (month >= 2 && month <= 4) return "spring";
